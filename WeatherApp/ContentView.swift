c21341f615c4284bc4b65ff82e1a732b34b55275
Oleh <tyzhai.o@ucu.edu.ua>
2019-10-25 09:42:09 +0300

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Олег on 25.10.2019.
+//  Copyright © 2019 Олег. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
